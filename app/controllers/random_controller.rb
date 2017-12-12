f97d913e1e0652a88b514976bc7c5fd0820c9537
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-12-12 16:10:18 -0600

    notes

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -3,7 +3,11 @@ class RandomController < ApplicationController
 
   # Begin here. perhaps destroy controller and start again. You only need to make the call in one controller and have another controller that creates the outfit and clothes. 
 
-  #I still need a controller for creating the outfit...and getting random clothes by checkoing the weather
+  #I still need a controller for creating the outfit...and getting random clothes by checkoing the weather http://blog.teamtreehouse.com/its-time-to-httparty https://www.learnhowtoprogram.com/rails/building-an-e-commerce-site-with-ajax-and-apis/making-api-calls-in-rails
+
+  # http://samples.openweathermap.org/data/2.5/forecast?zip=37013,us&appid=ea2449ceaa9da5d4322f0604b9c58bd9list.dt https://openweathermap.org/forecast5#zip
+
+  #https://www.youtube.com/watch?v=mwtKQlu7a08
 
   def outfit
   end
