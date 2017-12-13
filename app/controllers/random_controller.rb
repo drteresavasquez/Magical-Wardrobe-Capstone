26068ee683650eea124a62062fb3fe721af9dac1
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-12-13 15:58:13 -0600

    added random outfit generator

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -4,9 +4,27 @@ class RandomController < ApplicationController
     weather = OpenWeather.new("37013")
     response = weather.get_weather
 
-    temp = response['list'][0]['main']['temp']
-    weather = response['list'][1]['weather'][0]['main']
-    p "Temp = #{temp} and weather = #{weather}"
+    @temp = response['list'][0]['main']['temp']
+    if @temp >= 85
+      @temp_code = 3
+    elsif @temp >= 70
+      @temp_code = 4
+    elsif @temp >= 50
+      @temp_code = 2
+    else
+      @temp_code = 1
+    end
+
+    outfit = Outfit.where(user_id: current_user.id, temperature_type_id: @temp_code, active:true)
+    rand_outfit = outfit[Random.rand(outfit.count)]
+    if outfit.empty?
+      @selected = nil
+    else
+      @selected = rand_outfit
+    end
+
+    @weather = response['list'][1]['weather'][0]['main']
+    p "Temp: #{@temp}F Code: #{@temp_code} and weather = #{@weather}"
   end
 
   def clothes
