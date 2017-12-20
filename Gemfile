674acd1f65d35c55614912d3cb19e80cf3543875
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-12-20 16:19:21 -0600

    updated gemfile

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'turbolinks',              '5.0.1'
 gem 'jbuilder',                '2.7.0'
 gem 'font-awesome-rails'
 gem 'httparty'
+gem 'fog', '~> 1.42'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,33 +1,33 @@
 
-  CarrierWave.configure do |config|
-    if Rails.env.test?
-      config.storage :file
-      config.asset_host = 'http://localhost:3000'
-    else
-      config.storage = :fog
-      config.fog_use_ssl_for_aws = true
-      config.fog_directory  = ENV['S3_BUCKET']
-      config.fog_public     = true
-      config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
-      # config.asset_host = 'https://s3.amazonaws.com/website'
+  # CarrierWave.configure do |config|
+  #   if Rails.env.test?
+  #     config.storage :file
+  #     config.asset_host = 'http://localhost:3000'
+  #   else
+  #     config.storage = :fog
+  #     config.fog_use_ssl_for_aws = true
+  #     config.fog_directory  = ENV['S3_BUCKET']
+  #     config.fog_public     = true
+  #     config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+  #     # config.asset_host = 'https://s3.amazonaws.com/website'
       
-      config.fog_credentials = {
-        provider:               'AWS',
-        aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-        path_style:             ENV['FOG_PATH_STYLE']
-      }
-    end
-  end
-  
-  # if Rails.env.production?
-  #     CarrierWave.configure do |config|
-  #       config.fog_credentials = {
-  #         # Configuration for Amazon S3
-  #         :provider              => 'AWS',
-  #         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-  #         :aws_secret_access_key => ENV['S3_SECRET_KEY']
-  #       }
-  #       config.fog_directory     =  ENV['S3_BUCKET']
-  #     end
+  #     config.fog_credentials = {
+  #       provider:               'AWS',
+  #       aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+  #       aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+  #       path_style:             ENV['FOG_PATH_STYLE']
+  #     }
   #   end
+  # end
+  
+  if Rails.env.production?
+      CarrierWave.configure do |config|
+        config.fog_credentials = {
+          # Configuration for Amazon S3
+          :provider              => 'AWS',
+          :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+          :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        }
+        config.fog_directory     =  ENV['S3_BUCKET']
+      end
+    end
