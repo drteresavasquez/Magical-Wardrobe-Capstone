2b15c26322ffabb703e7e6802689a9a096dd7128
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-12-20 16:15:26 -0600

    stash

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,13 +6,13 @@
     else
       config.storage = :fog
       config.fog_use_ssl_for_aws = true
-      config.fog_directory  = ENV['FOG_DIRECTORY']
+      config.fog_directory  = ENV['S3_BUCKET']
       config.fog_public     = true
       config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
       # config.asset_host = 'https://s3.amazonaws.com/website'
       
       config.fog_credentials = {
-        provider:               ENV['FOG_PROVIDER'],
+        provider:               'AWS',
         aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
         path_style:             ENV['FOG_PATH_STYLE']
